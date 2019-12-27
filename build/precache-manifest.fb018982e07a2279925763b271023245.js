4a0c57a5a45d82df3c29c6ab08cf352d7fc134d3
dmitry.nevolin <dnevolin@eccentex.com>
2019-12-28 01:21:20 +0200

    add

---
+++ build/precache-manifest.fb018982e07a2279925763b271023245.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3042cc3e711fffd7587faf0dc17c56ee",
+    "url": "/tic-tac-toe/build/index.html"
+  },
+  {
+    "revision": "e2f5620a32233bda5e92",
+    "url": "/tic-tac-toe/build/static/js/2.caa523f8.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/tic-tac-toe/build/static/js/2.caa523f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8566de1643263298c2a9",
+    "url": "/tic-tac-toe/build/static/js/main.505da0f7.chunk.js"
+  },
+  {
+    "revision": "1268d2295fdf466b53d7",
+    "url": "/tic-tac-toe/build/static/js/runtime-main.8f05c02d.js"
+  }
+]);
