e3123a63639bbc28a0fa0764196c5064da62d096
dmitry.nevolin <dnevolin@eccentex.com>
2019-12-27 15:48:23 +0200

    add build folder

---
+++ build/precache-manifest.75d952c0fc99337236cc05d451c7eacc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d76834af63fd1e5f9e8d5ba6f99c61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2f5620a32233bda5e92",
+    "url": "/static/js/2.caa523f8.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.caa523f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7c734ccc7ea4adfc925",
+    "url": "/static/js/main.ae2bd72a.chunk.js"
+  },
+  {
+    "revision": "8a51a203f837dd63442e",
+    "url": "/static/js/runtime-main.fb877d2b.js"
+  }
+]);
